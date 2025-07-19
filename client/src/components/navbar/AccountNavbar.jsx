7dae71b7b79d4c67da3f93918ac2da6b725343c0
edisonyls <edisonyls@hotmail.com>
2025-07-19 18:23:36 +1000

    feat: update logos and navbar

--- client/src/components/navbar/AccountNavbar.jsx
+++ client/src/components/navbar/AccountNavbar.jsx
@@ -2,27 +2,31 @@ import React from "react";
 import { Link } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import FlutterDashIcon from "@mui/icons-material/FlutterDash";
+import Box from "@mui/material/Box";
 
 const AccountNavbar = () => {
   return (
-    <AppBar position="static" sx={{ background: "black" }}>
+    <AppBar position="static" sx={{ backgroundColor: "black" }}>
       <Toolbar>
-        <FlutterDashIcon />
-        <Typography
-          variant="h6"
+        <Box
           component={Link}
           to="/"
           sx={{
-            ml: 2,
             flexGrow: 1,
             textDecoration: "none",
             color: "inherit",
           }}
         >
-          YLSLC
-        </Typography>
+          <Box
+            component="img"
+            src="/full-logo.png"
+            alt="LC-Recorder"
+            sx={{
+              height: 45,
+              width: "auto",
+            }}
+          />
+        </Box>
       </Toolbar>
     </AppBar>
   );
